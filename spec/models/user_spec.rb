3d94da8e68b912330e11d1b8b17dab6350118b62
Andrew <ghobrial.andrew@gmail.com>
2013-11-09 22:29:35 -0500

    Make a basic User Model (including secure passwords)

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+  
+end
