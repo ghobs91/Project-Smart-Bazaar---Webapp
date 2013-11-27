3b4b0d243db6251efed983a4dc622a17cadbc64a
Andrew <ghobrial.andrew@gmail.com>
2013-11-27 14:23:05 -0500

    Home Page Edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"	
+      flash[:success] = "Welcome to the MatchCenter Alpha Test!"	
       redirect_to @user
     else
       render 'new'
