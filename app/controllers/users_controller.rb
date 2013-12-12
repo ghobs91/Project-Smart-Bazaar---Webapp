73d7210268cedc9f6d5321cf0ab9ed1ea5c9a4ce
Andrew <ghobrial.andrew@gmail.com>
2013-12-11 22:53:03 -0500

    allows delete user in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:destroy]
+  
 
   def show
     @user = User.find(params[:id])
