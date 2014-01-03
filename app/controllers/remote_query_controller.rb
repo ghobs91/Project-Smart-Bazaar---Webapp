1d02668318ac0acc0e4fcd9e4df61dd61f2d12c8
Chris <chrisonator@ubuntu.ubuntu-domain>
2014-01-03 14:42:18 -0500

    adds some of amazon api

---
+++ app/controllers/remote_query_controller.rb
@@ -0,0 +1,53 @@
+=begin class RemoteQueryController < ApplicationController
+	#include amazon helpers
+	include Amazon::AWS
+	include Amazon::AWS::Search
+
+	# GET /remote_query/:search 
+	# Searches Amazon's Product API for results using the keyword in :search
+	# Returns a list of the first 6 results or ["None"]
+	def index
+
+		# Prepare Search
+		is = ItemSearch.new( 'All', { 'Keywords' => params[:query] } )
+
+		# 'Large' required for images 
+		#  Could also be 'Small'' for just title & product group
+		rg = ResponseGroup.new( 'Large' )
+
+		# Perform the Search
+		req = Request.new
+		resp = req.search( is, rg ) 
+
+		results = []
+		resp["item_search_response"][0].items[0].item.each do |i|
+			# Grab the attributes we want
+			title = i.item_attributes.title[0].to_s[0,60]
+			group = i.item_attributes.product_group.to_s
+			image = amazon_image_set(i.image_sets[0].image_set)
+
+			# Add to results array
+			results << { :title => title, :group => group, :image => image }
+		end
+
+		# Return results 0-5 as JSON
+		render :json => results[0..5]
+
+		# If no results, Amazon::AWS throughs an error, so we can rescue
+		# TODO There's definitely a better way to handle this
+		rescue 
+			render :json => ["None"]
+	end
+
+	private
+
+	# Helper function, only argument is an image_set
+	# Returns the thumbnail_image url or ""
+	# Instead of thumbnail_image, could be small_image, large_image
+	def amazon_image_set(set)
+		set[0].thumbnail_image.url.to_s
+		rescue
+		 ""
+	end
+
+end               =end (this is just comment code)
---
+++ s/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_s_session',
+  :secret      => '936d108e8ad6ee57560154211a14b37a4f40bb376cbfc080c3f2e7307f7a41c86c8f3376bed84312573ca77f8ffde94877780ffe92799cd3b1254835adc0e5d1'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
